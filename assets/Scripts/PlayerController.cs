c3efd81ef2f1b64b83c4e71c01748d4b2d812c96
Jeffrey M. Johnson <jefjohms@gmail.com>
2014-05-11 10:29:10 -0700

    added GUI obvjects to keep score

--- assets/Scripts/PlayerController.cs
+++ assets/Scripts/PlayerController.cs
@@ -5,6 +5,17 @@ public class PlayerController : MonoBehaviour
 {
 
 		public float speed;
+		private int count;
+		public GUIText countText;
+		public GUIText winText;
+
+		void Start ()
+		{
+				count = 0;
+				setCountText ();
+				winText.text = "";
+		}
+
 		void FixedUpdate ()
 		{
 				float moveHorizontal = Input.GetAxis ("Horizontal");
@@ -13,12 +24,24 @@ public class PlayerController : MonoBehaviour
 				Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
 				rigidbody.AddForce (movement * speed * Time.deltaTime);
+
 		}
 
 		void OnTriggerEnter (Collider other)
 		{
 				if (other.gameObject.tag == "Pickup") {
 						other.gameObject.SetActive (false);
+						count++;
+						setCountText ();
+
+						if (count == 14) {
+								winText.text = "You Win!";
+						}
 				}
+		}
+
+		void setCountText ()
+		{
+				countText.text = "Count: " + count.ToString ();
 		}
 }
